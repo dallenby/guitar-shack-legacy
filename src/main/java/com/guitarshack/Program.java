3a9f32db8bfb0cd057c6d42beab650197a1ba637
Theo Brown <tbrow403@ford.com>
2021-12-10 11:55:55 +0000

    tests passed

--- src/main/java/com/guitarshack/Program.java
+++ src/main/java/com/guitarshack/Program.java
@@ -2,12 +2,17 @@ package com.guitarshack;
 
 public class Program {
 
-    private static StockMonitor monitor = new StockMonitor(product -> {
-        // We are faking this for now
-        System.out.println(
-                "You need to reorder product " + product.getId() +
-                        ". Only " + product.getStock() + " remaining in stock");
-    }, new Request("https://6hr1390c1j.execute-api.us-east-2.amazonaws.com/default/product"), new Request("https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com/default/sales"));
+    private static StockMonitor monitor;
+
+    static {
+        Request salesRequest = new Request("https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com/default/sales");
+        monitor = new StockMonitor(product -> {
+            // We are faking this for now
+            System.out.println(
+                    "You need to reorder product " + product.getId() +
+                            ". Only " + product.getStock() + " remaining in stock");
+        }, new Request("https://6hr1390c1j.execute-api.us-east-2.amazonaws.com/default/product"), salesRequest);
+    }
 
     public static void main(String[] args) {
         int productId = Integer.parseInt(args[0]);
