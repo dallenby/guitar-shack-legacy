cc30da082033b90dd0cc537f00ad0c0816281cc1
Daniel Allenby <dallenby@ford.com>
2021-12-10 13:01:52 +0000

    tests passed

--- src/main/java/com/guitarshack/ReorderThreshold.java
+++ src/main/java/com/guitarshack/ReorderThreshold.java
@@ -1,6 +1,7 @@
 package com.guitarshack;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class ReorderThreshold {
     private final Calendar calendar;
@@ -17,5 +18,14 @@ public class ReorderThreshold {
 
     public SalesHistory getSalesHistory() {
         return salesHistory;
+    }
+
+    int getReorderThreshold(Product product) {
+        getCalendar().add(Calendar.YEAR, -1);
+        Date startDate = getCalendar().getTime();
+        getCalendar().add(Calendar.DATE, 30);
+        Date endDate = getCalendar().getTime();
+        SalesTotal total = getSalesHistory().getSalesTotal(product, endDate, startDate);
+        return (int) ((double) (total.getTotal() / 30) * product.getLeadTime());
     }
 }
--- src/main/java/com/guitarshack/StockMonitor.java
+++ src/main/java/com/guitarshack/StockMonitor.java
@@ -1,7 +1,6 @@
 package com.guitarshack;
 
 import java.util.Calendar;
-import java.util.Date;
 
 public class StockMonitor {
     private final Alert alert;
@@ -18,16 +17,8 @@ public class StockMonitor {
 
     public void productSold(int productId, int quantity) {
         Product product = warehouse.getProduct(productId);
-        if(product.getStock() - quantity <= getReorderThreshold(product, new ReorderThreshold(calendar, salesHistory)))
+        if(product.getStock() - quantity <= new ReorderThreshold(calendar, salesHistory).getReorderThreshold(product))
             alert.send(product);
     }
 
-    private int getReorderThreshold(Product product, ReorderThreshold reorderThreshold) {
-        reorderThreshold.getCalendar().add(Calendar.YEAR, -1);
-        Date startDate = reorderThreshold.getCalendar().getTime();
-        reorderThreshold.getCalendar().add(Calendar.DATE, 30);
-        Date endDate = reorderThreshold.getCalendar().getTime();
-        SalesTotal total = reorderThreshold.getSalesHistory().getSalesTotal(product, endDate, startDate);
-        return (int) ((double) (total.getTotal() / 30) * product.getLeadTime());
-    }
 }
