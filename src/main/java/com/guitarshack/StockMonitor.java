a9c215a51d234b6f6e232514e8926ab861046475
Daniel Allenby <dallenby@ford.com>
2021-12-10 10:24:19 +0000

    Extracted get request into a method

--- src/main/java/com/guitarshack/StockMonitor.java
+++ src/main/java/com/guitarshack/StockMonitor.java
@@ -26,6 +26,44 @@ public class StockMonitor {
         Map<String, Object> params = new HashMap<>() {{
             put("id", productId);
         }};
+        String result = get(baseURL, params);
+        Product product = new Gson().fromJson(result, Product.class);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(Calendar.getInstance().getTime());
+        Date endDate = calendar.getTime();
+        calendar.add(Calendar.DATE, -30);
+        Date startDate = calendar.getTime();
+        DateFormat format = new SimpleDateFormat("M/d/yyyy");
+        Map<String, Object> params1 = new HashMap<>(){{
+            put("productId", product.getId());
+            put("startDate", format.format(startDate));
+            put("endDate", format.format(endDate));
+            put("action", "total");
+        }};
+        String url = "https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com/default/sales";
+        String paramString1 = "?";
+
+        for (String key : params1.keySet()) {
+            paramString1 += key + "=" + params1.get(key).toString() + "&";
+        }
+        HttpRequest request1 = HttpRequest
+                .newBuilder(URI.create(url + paramString1))
+                .build();
+        String result1 = "";
+        HttpClient httpClient1 = HttpClient.newHttpClient();
+        HttpResponse<String> response1 = null;
+        try {
+            response1 = httpClient1.send(request1, HttpResponse.BodyHandlers.ofString());
+            result1 = response1.body();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        SalesTotal total = new Gson().fromJson(result1, SalesTotal.class);
+        if(product.getStock() - quantity <= (int) ((double) (total.getTotal() / 30) * product.getLeadTime()))
+            alert.send(product);
+    }
+
+    private String get(String baseURL, Map<String, Object> params) {
         String paramString = "?";
 
         for (String key : params.keySet()) {
@@ -43,39 +81,6 @@ public class StockMonitor {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        Product product = new Gson().fromJson(result, Product.class);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(Calendar.getInstance().getTime());
-        Date endDate = calendar.getTime();
-        calendar.add(Calendar.DATE, -30);
-        Date startDate = calendar.getTime();
-        DateFormat format = new SimpleDateFormat("M/d/yyyy");
-        Map<String, Object> params1 = new HashMap<>(){{
-            put("productId", product.getId());
-            put("startDate", format.format(startDate));
-            put("endDate", format.format(endDate));
-            put("action", "total");
-        }};
-        String paramString1 = "?";
-
-        for (String key : params1.keySet()) {
-            paramString1 += key + "=" + params1.get(key).toString() + "&";
-        }
-        HttpRequest request1 = HttpRequest
-                .newBuilder(URI.create("https://gjtvhjg8e9.execute-api.us-east-2.amazonaws.com/default/sales" + paramString1))
-                .build();
-        String result1 = "";
-        HttpClient httpClient1 = HttpClient.newHttpClient();
-        HttpResponse<String> response1 = null;
-        try {
-            response1 = httpClient1.send(request1, HttpResponse.BodyHandlers.ofString());
-            result1 = response1.body();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        SalesTotal total = new Gson().fromJson(result1, SalesTotal.class);
-        if(product.getStock() - quantity <= (int) ((double) (total.getTotal() / 30) * product.getLeadTime()))
-            alert.send(product);
+        return result;
     }
-
 }
